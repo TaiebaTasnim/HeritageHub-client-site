22094be364bda45ad08184589c8c626ebb4c3d2c
TaiebaTasnim <tasnimtaieba111@gmail.com>
2024-12-26 21:09:37 +0600

    error page design done

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,29 +1,33 @@
+import Lottie from 'lottie-react';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate, useRouteError } from 'react-router-dom';
+import errorlottie from "../assets/Lottie/error.json"
 
 const ErrorPage = () => {
       const error=useRouteError()
       const navigate=useNavigate()
       
       return (
-            <div className='flex flex-col justify-center items-center min-h-screen gap-3 bg-black '>
+            <div className='flex flex-col justify-center items-center min-h-screen gap-2 bg-white '>
                   <Helmet>
                   <title>Error Page</title>
                   </Helmet>
-                  <h1 className='text-5xl text-white'>Ooops!!!</h1>
-                  <p className='text-white'>Sorry, an unexpected error has occurred.</p>
-                 <p className='text-white'>
+                  <div >
+                        <Lottie animationData={errorlottie}></Lottie>
+
+                  </div>
+                  <h1 className='text-4xl text-black'>Ooops!!!</h1>
+                  <p className='text-black'>Sorry, an unexpected error has occurred.</p>
+                 <p className='text-black'>
                     <i>{error.statusText || error.message} </i>
                 </p>
-                <button onClick={()=>navigate('/')} className="flex items-center gap-2 py-3 px-6 bg-[#e20934] text-white rounded-lg font-semibold transition duration-500 ease-in-out relative overflow-hidden group">
-                  <span className="absolute inset-0 bg-gradient-to-r from-[#e20934] to-black opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-                  <span className="relative group-hover:text-white transition duration-500 ease-in-out">
-                    Go Back
-                    
-                  </span>
-                  
-                </button>
+                <button onClick={()=>navigate('/')} className="py-3 px-6 bg-[#000029] text-white rounded-lg font-semibold transition duration-500 ease-in-out relative overflow-hidden group text-center">
+              <span className="absolute inset-0 bg-gradient-to-r from-[#000029] to-[#00FFFF] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+              <span className="relative group-hover:text-white transition duration-500 ease-in-out text-center">
+                Go Back
+              </span>
+            </button>
             </div>
       );
 };
